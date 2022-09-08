d2ae0a0f90173f1cf27c6bad3779d857a077860b
Filip Skoczen <philipskoczen@gmail.com>
2022-09-08 19:36:30 +0200

    Add prefixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { RouteDefinition, useRoutes } from '@solidjs/router';
 import { Component, lazy } from 'solid-js';
-const pages = import.meta.glob('./pages/**/*') as Record<string, () => Promise<{ default: Component }>>
 
-// './pages/index.tsx'
+const pages = import.meta.glob('./pages/**/*') as Record<string, () => Promise<{ default: Component }>>;
 
 const getPath = (path: string) => path
   .replace('./pages/', '') // Remove ./pages/ from start
-  .replaceAll('index','') // Remove indexes from path
+  .replaceAll('index', '') // Remove indexes from path
   .replace(/\.[tj]sx/, '') // Remove .tsx and .jsx
-  .replace(/\[.+]/, (param) => param.slice(1, -1)) // Change [param] to :param
+  .replace(/\[.+]/, (param) => param.slice(1, -1)); // Change [param] to :param
 
 const routes = Object.entries(pages).map(([path, component]): RouteDefinition => ({
-  path: getPath(path), component: lazy(component)
-}))
-
-console.log(routes);
+  path: getPath(path), component: lazy(component),
+}));
 
 function App() {
   const Routes = useRoutes(routes);
